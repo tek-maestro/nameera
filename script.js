5a376a2d525d1b061e5766a09cbc20111b02255b
tek-maestro <148618096+tek-maestro@users.noreply.github.com>
2024-10-19 22:22:42 +0600

    Fixed leap years

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ setInterval(updateAge, 1000);
 
 function updateAge() {
     const now = new Date();
-    age.textContent = describeTime(now - BIRTH_DATE);
+    let duration = getDuration(BIRTH_DATE, duration);
+    age.textContent = describeTime(duration);
 }
 
 function describeTime(timeInMillis) {
     const units = [365, 24, 60, 60, 1000];
     const unitNames = ["year", "day", "hour", "minute", "second"];
-    let total = 365*24*60*60*1000;
+    let total = 365 * 24 * 60 * 60 * 1000;
     let results = [];
     for (let unit of units) {
         results.push(parseInt(timeInMillis/total));
@@ -20,6 +21,16 @@ function describeTime(timeInMillis) {
     }
     let desc = "";
     for (let i = 0; i < results.length; i++)
-        desc += `${results[i]} ${unitNames[i]}${results[i]==1?"":"s"} `;
+        desc += `${results[i]} ${unitNames[i]}${results[i]==1? "" : "s"} `;
     return desc;
+}
+
+function getDuration(t1, t2) {
+    const isLeapYear = year => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    const dayInMillis = 24 * 60 * 60 * 1000;
+    let duration = t2 - t1;
+    let y1 = t1.getFullYear(), y2 = t2.getFullYear();
+    for (let y = y1 + (t1.getMonth() > 1); y <= y2 - (t2.getMonth() <= 1); y++)
+        if (isLeapYear(y)) duration -= dayInMillis;
+    return duration;
 }
